bc4f3ae6d1728625cec9eb3c2324b2a32d9d15d1
Goran SD <g.cvetic23@gmail.com>
2024-04-02 10:04:30 -0700

    Add cSpell.words to .vscode/settings.json and configure remotePatterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        domain: "bytegrad.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
