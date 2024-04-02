7cd0a8771f59bebacec2cfb619ed532629321ffd
Goran SD <g.cvetic23@gmail.com>
2024-04-02 11:43:33 -0700

    Update page layout and add styling

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-[#5DC9A8] min-h-screen flex items-center justify-center gap-10">
+    <main className="bg-[#5DC9A8] min-h-screen flex xl:flex-row flex-col items-center justify-center gap-10">
       <Image
         src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
         alt="Pet Tracker App Preview"
@@ -15,7 +15,7 @@ export default function Home() {
           Manage your <span className="font-extrabold"> pet daycare </span> with
           ease
         </h1>
-        <p>
+        <p className="text-2xl font-medium max-w-[600px]">
           Use Pet Tracker to easily keep track of pets under your care. Get a
           lifetime access for $299.
         </p>
