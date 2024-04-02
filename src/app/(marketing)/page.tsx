9d99a8f43c503f3d1b2d2b0b7e179dd499b31465
Goran SD <g.cvetic23@gmail.com>
2024-04-02 10:02:51 -0700

    Add image to Home page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="bg-[#5DC9A8] min-h-screen">Home Page</main>;
+  return (
+    <main className="bg-[#5DC9A8] min-h-screen">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="Pet Tracker App Preview"
+        width={519}
+        height={472}
+      />
+    </main>
+  );
 }
