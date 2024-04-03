3ca31101826a42189f8bdee8ccccbf19f0e6805d
Goran SD <g.cvetic23@gmail.com>
2024-04-03 09:18:14 -0700

    Update dashboard page layout and content

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,13 +1,17 @@
 const Page = () => {
   return (
     <main>
-      <div>
+      <div className="flex justify-between items-center text-white py-8">
         <section className="text-medium text-2xl leading-6">
           <h1>
-            Pet <span className="font-semibold">Stay</span>
+            Pet<span className="font-semibold">Stay</span>
           </h1>
+          <p>Manage your pet daycare with ease.</p>
         </section>
-        <section></section>
+        <section>
+          <p className="text-2xl font-bold leading-6">2</p>
+          <p className="opacity-80">Current Guests</p>
+        </section>
       </div>
     </main>
   );
