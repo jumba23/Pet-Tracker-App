0e51659024d90f4cebb55c8799816b245e4fbc21
Goran SD <g.cvetic23@gmail.com>
2024-04-03 09:05:10 -0700

    Refactor app-footer component to include copyright information

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,11 @@
 const AppFooter = () => {
-  return <div>AppFooter</div>;
+  return (
+    <footer className="border-t border-black/5 py-5 mt-auto">
+      <small className="opacity-50">
+        &copy; {new Date().getFullYear()} All rights reserved by Soft Solutions.
+      </small>
+    </footer>
+  );
 };
 
 export default AppFooter;
