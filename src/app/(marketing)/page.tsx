8b198f90273c3212db8880e83bf8be6624ec3739
Goran SD <g.cvetic23@gmail.com>
2024-04-03 09:12:55 -0700

    Update alt text in marketing page component

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="bg-[#5DC9A8] min-h-screen flex xl:flex-row flex-col items-center justify-center gap-10">
       <Image
         src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
-        alt="Pet Tracker App Preview"
+        alt="PetStay App Preview"
         width={519}
         height={472}
       />
@@ -20,7 +20,7 @@ export default function Home() {
           ease
         </h1>
         <p className="text-2xl font-medium max-w-[600px]">
-          Use Pet Tracker to easily keep track of pets under your care. Get a
+          Use PetStay app to easily keep track of pets under your care. Get a
           lifetime access for $299.
         </p>
         <div className="mt-10 space-x-3">
