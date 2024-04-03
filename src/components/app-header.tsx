8927d014e6c69dc85ae41d560885d662e83392c9
Goran SD <g.cvetic23@gmail.com>
2024-04-03 09:05:14 -0700

    Add active link styling to AppHeader

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./logo";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const routes = [
   {
@@ -13,15 +17,27 @@ const routes = [
 ];
 
 const AppHeader = () => {
+  const activePathName = usePathname();
+
   return (
-    <header className="flex justify-between border-b border-white/10 py-2">
+    <header className="flex justify-between items-center border-b border-white/10 py-2">
       <Logo />
 
       <nav>
         <ul className="flex gap-2 text-xs">
           {routes.map((route) => (
             <li key={route.path}>
-              <Link href={route.path}>{route.label}</Link>
+              <Link
+                className={cn(
+                  "text-white/70 rounded-sm px-2 py-1 hover:text-white focus:text-white transition",
+                  {
+                    " bg-black/10 text-white": route.path === activePathName,
+                  }
+                )}
+                href={route.path}
+              >
+                {route.label}
+              </Link>
             </li>
           ))}
         </ul>
