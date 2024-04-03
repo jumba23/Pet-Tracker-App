64971f59892559d2342534fa56d8216751029eba
Goran SD <g.cvetic23@gmail.com>
2024-04-02 23:16:14 -0700

    Add navigation links to AppHeader component and update background pattern

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,5 +1,33 @@
+import Link from "next/link";
+import Logo from "./logo";
+
+const routes = [
+  {
+    label: "Dashboard",
+    path: "/app/dashboard",
+  },
+  {
+    label: "Account",
+    path: "/app/account",
+  },
+];
+
 const AppHeader = () => {
-  return <div>AppHeader</div>;
+  return (
+    <header className="flex justify-between">
+      <Logo />
+
+      <nav>
+        <ul className="flex gap-2">
+          {routes.map((route) => (
+            <li key={route.path}>
+              <Link href={route.path}>{route.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default AppHeader;
--- src/components/background-pattern.tsx
+++ src/components/background-pattern.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BackgroundPattern = () => {
-  return <div>BackgroundPattern</div>;
+  return <div className="bg-[#2C9676] h-[300px] w-full absolute top-0 -z-10" />;
 };
 
 export default BackgroundPattern;
