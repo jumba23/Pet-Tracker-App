73e3e226231e5304f81a3ea40beac7cadff35dfe
Goran SD <g.cvetic23@gmail.com>
2024-04-02 23:09:58 -0700

    Add layout components

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -1,5 +1,17 @@
-const Layout = ({ children }) => {
-  return <div>HEADER {children}</div>;
+import AppFooter from "@/components/app-footer";
+import AppHeader from "@/components/app-header";
+import BackgroundPattern from "@/components/background-pattern";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <BackgroundPattern />
+
+      <AppHeader />
+      {children}
+      <AppFooter />
+    </>
+  );
 };
 
 export default Layout;
---
+++ src/components/background-pattern.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BackgroundPattern = () => {
+  return <div>BackgroundPattern</div>;
+};
+
+export default BackgroundPattern;
