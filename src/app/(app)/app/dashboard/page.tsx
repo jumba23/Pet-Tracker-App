36bbdbc77d4070e821bbec7c72ae56c5088ac8f5
Goran SD <g.cvetic23@gmail.com>
2024-04-03 09:12:49 -0700

    Refactor dashboard page layout

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,16 @@
 const Page = () => {
-  return <div>Dashboard Page</div>;
+  return (
+    <main>
+      <div>
+        <section className="text-medium text-2xl leading-6">
+          <h1>
+            Pet <span className="font-semibold">Stay</span>
+          </h1>
+        </section>
+        <section></section>
+      </div>
+    </main>
+  );
 };
 
 export default Page;
