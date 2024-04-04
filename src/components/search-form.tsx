e9e1d76e703eba13aec203670d3020584c86ca10
Goran SD <g.cvetic23@gmail.com>
2024-04-04 16:17:35 -0700

    Update search form input styling

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,7 +1,11 @@
 const SearchForm = () => {
   return (
     <form className="h-full w-full">
-      <input type="text" className="h-full w-full bg-white/20" />
+      <input
+        type="search"
+        className="h-full w-full bg-white/20 rounded-md px-5 outline-none transition focus:bg-white/50 hover:bg-white/30 placeholder:text-white/50"
+        placeholder="Search pets"
+      />
     </form>
   );
 };
