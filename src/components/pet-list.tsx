baff4ba082cad730fd1ee18c0179aba4d5d30632
Goran SD <g.cvetic23@gmail.com>
2024-04-04 09:54:29 -0700

    Add Benjamin's pet image to the pet list

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -15,6 +15,18 @@ const PetList = () => {
           <p className="font-semibold">Benjamin</p>
         </button>
       </li>
+      <li>
+        <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Benjamin</p>
+        </button>
+      </li>
     </ul>
   );
 };
