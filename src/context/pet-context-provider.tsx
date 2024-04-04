601c6aa3abb3c1d46508123e230525cbb9019b72
Goran SD <g.cvetic23@gmail.com>
2024-04-04 10:27:33 -0700

    Refactor PetContextProvider to use React context and state

--- src/context/pet-context-provider.tsx
+++ src/context/pet-context-provider.tsx
@@ -1,5 +1,21 @@
-const PetContextProvider = () => {
-  return <div>PetContextProvider</div>;
+import { createContext, useState } from "react";
+
+export const PetContext = createContext(null);
+
+const PetContextProvider = ({ children }) => {
+  const [pets, setPets] = useState([]);
+  const [selectedPetId, setSelectedPetId] = useState(null);
+
+  return (
+    <PetContext.Provider
+      value={{
+        pets,
+        selectedPetId,
+      }}
+    >
+      {children}
+    </PetContext.Provider>
+  );
 };
 
 export default PetContextProvider;
