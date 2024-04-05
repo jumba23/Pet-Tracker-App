360cfaa04ac3b550335ec8a217a721cfadddab2a
Goran SD <g.cvetic23@gmail.com>
2024-04-05 09:52:38 -0700

    Refactor search context provider to pet context provider

--- src/context/search-context-provider.tsx
+++ src/context/search-context-provider.tsx
@@ -1,5 +1,52 @@
-const SearchContextProvider = () => {
-  return <div>SearchContextProvider</div>;
+"use client";
+
+import { Pet } from "@/lib/types";
+import { createContext, useState } from "react";
+
+type TPetContext = {
+  pets: Pet[];
+  selectedPetId: string | null;
+  selectedPet: Pet | undefined;
+  numberOfPets: number;
+  handleSetSelectedPetId: (id: string) => void;
 };
 
-export default SearchContextProvider;
+export const PetContext = createContext<TPetContext | null>(null);
+
+type PetContextProviderProps = {
+  data: Pet[];
+  children: React.ReactNode;
+};
+
+const PetContextProvider = ({ data, children }: PetContextProviderProps) => {
+  // state
+  const [pets, setPets] = useState(data);
+  const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
+
+  //derived state
+  const selectedPet = pets.find((pet) => pet.id === selectedPetId);
+  const numberOfPets = pets.length;
+
+  //event handlers / actions
+
+  //use handle function to set selected pet id
+  const handleSetSelectedPetId = (id: string) => {
+    setSelectedPetId(id);
+  };
+
+  return (
+    <PetContext.Provider
+      value={{
+        pets,
+        selectedPetId,
+        handleSetSelectedPetId,
+        selectedPet,
+        numberOfPets,
+      }}
+    >
+      {children}
+    </PetContext.Provider>
+  );
+};
+
+export default PetContextProvider;
