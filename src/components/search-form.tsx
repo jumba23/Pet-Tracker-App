0e5f210f9cfdb2609a271ffde43f14b30a952f1e
Goran SD <g.cvetic23@gmail.com>
2024-04-05 23:28:37 -0700

    Refactor search form component to use handleChangeSearchQuery

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -3,7 +3,7 @@
 import { useSearchContext } from "@/lib/hooks";
 
 const SearchForm = () => {
-  const { searchQuery } = useSearchContext();
+  const { searchQuery, handleChangeSearchQuery } = useSearchContext();
 
   return (
     <form className="h-full w-full">
@@ -12,7 +12,7 @@ const SearchForm = () => {
         className="h-full w-full bg-white/20 rounded-md px-5 outline-none transition focus:bg-white/50 hover:bg-white/30 placeholder:text-white/50"
         placeholder="Search pets"
         value={searchQuery}
-        onChange={(e) => console.log(e.target.value)}
+        onChange={(e) => handleChangeSearchQuery(e.target.value)}
       />
     </form>
   );
