bc34682f0c9f258b10f1d65ede1743c298193100
Goran SD <g.cvetic23@gmail.com>
2024-04-05 23:41:37 -0700

    Update H1 component styling

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -4,7 +4,7 @@ import H1 from "@/components/h1";
 const Page = () => {
   return (
     <main>
-      <H1>Your Account</H1>
+      <H1 className="my-8">Your Account</H1>
 
       <ContentBlock>
         <p>Logged in as ...</p>
--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -1,4 +1,9 @@
-const H1 = ({ children }: { children: React.ReactNode }) => {
+type H1Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const H1 = ({ children, className }: H1Props) => {
   return <h1 className="text-medium text-2xl leading-6">{children}</h1>;
 };
 
