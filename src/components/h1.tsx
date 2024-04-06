ef96c519dc3f7849057b2aac9d684a8015956326
Goran SD <g.cvetic23@gmail.com>
2024-04-05 23:42:55 -0700

    Refactor H1 component to use dynamic class names

--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -1,10 +1,16 @@
+import { cn } from "@/lib/utils";
+
 type H1Props = {
   children: React.ReactNode;
   className?: string;
 };
 
 const H1 = ({ children, className }: H1Props) => {
-  return <h1 className="text-medium text-2xl leading-6">{children}</h1>;
+  return (
+    <h1 className={cn("text-medium text-2xl leading-6", className)}>
+      {children}
+    </h1>
+  );
 };
 
 export default H1;
