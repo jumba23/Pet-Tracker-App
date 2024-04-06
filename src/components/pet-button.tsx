7ac922808666d7cd9f3340b9cc13f5630cbb35dd
Goran SD <g.cvetic23@gmail.com>
2024-04-06 00:48:06 -0700

    Add PetButton component and update button styles

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,3 +1,4 @@
+import { PlusIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 
 type PetButtonProps = {
@@ -5,6 +6,21 @@ type PetButtonProps = {
 };
 
 const PetButton = ({ actionType }: PetButtonProps) => {
+  if (actionType === "add")
+    return (
+      <Button size="icon">
+        <PlusIcon className="h-6 w-6" />{" "}
+      </Button>
+    );
+
+  if (actionType === "edit") {
+    return <Button variant="secondary">Edit </Button>;
+  }
+
+  if (actionType === "checkout") {
+    return <Button variant="secondary">Checkout </Button>;
+  }
+
   return <Button>Edit </Button>;
 };
 
