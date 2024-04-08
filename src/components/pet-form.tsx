8659cc6c571e43dc7986ef099a082912fee53fb6
Goran SD <g.cvetic23@gmail.com>
2024-04-08 10:16:58 -0700

    Update PetForm component with default values

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -44,28 +44,51 @@ const PetForm = ({ actionType, onFormSubmission }: PetFormProps) => {
             type="text"
             name="name"
             required
-            defaultValue={actionType === "edit" ? "Fluffy" : ""}
+            defaultValue={actionType === "edit" ? selectedPet?.name : ""}
           />
         </div>
 
         <div className="space-y-1">
           <Label htmlFor="ownerName">Owner Name</Label>
-          <Input id="name" type="text" name="ownerName" required />
+          <Input
+            id="name"
+            type="text"
+            name="ownerName"
+            required
+            defaultValue={actionType === "edit" ? selectedPet?.ownerName : ""}
+          />
         </div>
 
         <div className="space-y-1">
           <Label htmlFor="imageUrl">Image Url</Label>
-          <Input id="name" type="text" name="imageUrl" />
+          <Input
+            id="name"
+            type="text"
+            name="imageUrl"
+            defaultValue={actionType === "edit" ? selectedPet?.imageUrl : ""}
+          />
         </div>
 
         <div className="space-y-1">
           <Label htmlFor="age">Age</Label>
-          <Input id="name" type="text" name="age" required />
+          <Input
+            id="name"
+            type="text"
+            name="age"
+            required
+            defaultValue={actionType === "edit" ? selectedPet?.age : ""}
+          />
         </div>
 
         <div className="space-y-1">
           <Label htmlFor="notes">Notes</Label>
-          <Textarea id="name" rows={3} name="notes" required />
+          <Textarea
+            id="name"
+            rows={3}
+            name="notes"
+            required
+            defaultValue={actionType === "edit" ? selectedPet?.notes : ""}
+          />
         </div>
       </div>
 
