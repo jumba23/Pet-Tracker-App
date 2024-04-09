cbe1dbadfe5b21a1bf64a1283c7ed41ddb261094
Goran SD <g.cvetic23@gmail.com>
2024-04-09 10:07:46 -0700

    Refactor PetForm component to use PetFormBtn for form submission in pet-form.tsx

--- src/components/pet-form-btn.tsx
+++ src/components/pet-form-btn.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button } from "./ui/button";
+import { useFormStatus } from "react-dom";
 
-const PetFormBtn = () => {
-  return <div>PetFormBtn</div>;
+const PetFormBtn = ({ actionType }) => {
+  // useFormStatus comes with server action in parent component
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" className="mt-5 self-end" disabled={pending}>
+      {actionType === "add" ? "Add a new pet" : "Edit Pet"}
+    </Button>
+  );
 };
 
 export default PetFormBtn;
--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -6,6 +6,7 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { addPet } from "@/actions/actions";
+import PetFormBtn from "./pet-form-btn";
 
 type PetFormProps = {
   actionType: "add" | "edit";
@@ -79,9 +80,7 @@ const PetForm = ({ actionType, onFormSubmission }: PetFormProps) => {
         </div>
       </div>
 
-      <Button type="submit" className="mt-5 self-end">
-        {actionType === "add" ? "Add a new pet" : "Edit Pet"}
-      </Button>
+      <PetFormBtn actionType={actionType} />
     </form>
   );
 };
