26bdcdf85cdbb8d7e1a440bb2fc5b6feb709dac4
Goran SD <g.cvetic23@gmail.com>
2024-04-09 10:07:39 -0700

    Add sleep function to utils.ts for simulating API response delay

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// this function is used to simulate a delay in the API response
 export const sleep = async (ms: number) => {
   return await new Promise((resolve) => {
     setTimeout(resolve, ms);
