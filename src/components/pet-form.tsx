db48a4d64e5404044ec466f3f4ba846f05e52b25
Goran SD <g.cvetic23@gmail.com>
2024-04-09 09:48:39 -0700

    Refactor PetForm component to use async/await for form submission in pet-form.tsx

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -16,7 +16,13 @@ const PetForm = ({ actionType, onFormSubmission }: PetFormProps) => {
   const { selectedPet } = usePetContext();
 
   return (
-    <form action={addPet} className="flex flex-col">
+    <form
+      action={async (formData) => {
+        await addPet(formData);
+        onFormSubmission();
+      }}
+      className="flex flex-col"
+    >
       <div className="space-y-3">
         <div className="space-y-1">
           <Label htmlFor="name">Name</Label>
