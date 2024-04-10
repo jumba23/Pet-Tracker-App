470a4c2191fcdf9d0843746eadd3b50cb060e94f
Goran SD <g.cvetic23@gmail.com>
2024-04-10 10:01:47 -0700

    Refactor editPet action to include error handling and default image URL in actions.ts

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -25,23 +25,32 @@ export const addPet = async (formData) => {
       message: "Failed to add pet",
     };
   }
-
+  // re-render the layout to show the new pet
   revalidatePath("/app", "layout");
 };
 
 export const editPet = async (petId, formData) => {
-  prisma?.pet.update({
-    where: {
-      id: petId,
-    },
-    data: {
-      name: formData.get("name"),
-      ownerName: formData.get("ownerName"),
-      imageUrl:
-        formData.get("imageUrl") ||
-        "      https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png      ",
-      age: parseInt(formData.get("age")),
-      notes: formData.get("notes"),
-    },
-  });
+  try {
+    await prisma?.pet.update({
+      where: {
+        id: petId,
+      },
+      data: {
+        name: formData.get("name"),
+        ownerName: formData.get("ownerName"),
+        imageUrl:
+          formData.get("imageUrl") ||
+          "      https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png      ",
+        age: parseInt(formData.get("age")),
+        notes: formData.get("notes"),
+      },
+    });
+  } catch (error) {
+    return {
+      message: "Failed to edit pet",
+    };
+  }
+
+  // re-render the layout to show the updated pet
+  revalidatePath("/app", "layout");
 };
