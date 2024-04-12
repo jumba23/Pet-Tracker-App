fd3f67e51b506c03ab5068908624ee2b71a9b284
Goran SD <g.cvetic23@gmail.com>
2024-04-11 22:52:25 -0700

    Refactor PetFormBtn component to use type annotations for props and remove unused import

--- src/components/pet-form-btn.tsx
+++ src/components/pet-form-btn.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Button } from "./ui/button";
-import { useFormStatus } from "react-dom";
 
-const PetFormBtn = ({ actionType }) => {
+type PetFormBtnProps = {
+  actionType: "add" | "edit";
+};
+
+const PetFormBtn = ({ actionType }: PetFormBtnProps) => {
   // useFormStatus comes with server action in parent component
-  const { pending } = useFormStatus();
+  // const { pending } = useFormStatus();
 
   return (
-    <Button type="submit" className="mt-5 self-end" disabled={pending}>
+    <Button type="submit" className="mt-5 self-end">
       {actionType === "add" ? "Add a new pet" : "Edit Pet"}
     </Button>
   );
