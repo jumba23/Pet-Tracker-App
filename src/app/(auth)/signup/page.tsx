4d1e0897e11719cebba784f7ec851abe537cfdd8
Goran SD <g.cvetic23@gmail.com>
2024-04-14 08:39:17 -0700

    Refactor login page to use Link component for sign up link and add className to H1 component in page.tsx

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,26 @@
-const SignUp = () => {
-  return <div>Sign up page</div>;
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
+
+const Login = () => {
+  return (
+    <main>
+      <H1 className="mb-5 text-center">Sign Up</H1>
+
+      <AuthForm />
+
+      <p className="mt-6 text-sm text-zinc-500">
+        Already have an account?{" "}
+        {/* <a href="/sign-up" className="text-blue-500">
+          Sign up
+        </a> */}
+        {/* Replace the anchor tag with the Link component. Link component will pre-fetch */}
+        <Link href="/login" className="font-medium">
+          Log in
+        </Link>
+      </p>
+    </main>
+  );
 };
 
-export default SignUp;
+export default Login;
