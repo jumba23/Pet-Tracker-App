aa5f5a4301a17fae7de22c57b9ff6413ddbc1cb4
Goran SD <g.cvetic23@gmail.com>
2024-04-14 08:25:44 -0700

    Add AuthForm component and refactor login page to use H1 component for heading in page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,19 @@
+import AuthForm from "@/components/auth-form";
 import H1 from "@/components/h1";
 
 const Login = () => {
   return (
     <main>
       <H1>Login Page</H1>
+
+      <AuthForm />
+
+      <p>
+        No account yet?{" "}
+        <a href="/sign-up" className="text-blue-500">
+          Sign up
+        </a>
+      </p>
     </main>
   );
 };
