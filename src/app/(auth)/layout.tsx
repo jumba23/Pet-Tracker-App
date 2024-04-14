be183d68a2ef756fe5df3a55271e543ff3f57db4
Goran SD <g.cvetic23@gmail.com>
2024-04-14 08:20:18 -0700

    Refactor auth layout component to include logo in layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,12 @@
+import Logo from "@/components/logo";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Logo />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
