27645ce4334bc8a24edcb52db5266f0549568b32
Goran SD <g.cvetic23@gmail.com>
2024-04-14 08:32:14 -0700

    Refactor AuthForm component to include className and space-y styles in auth-form.tsx

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -3,13 +3,13 @@ import { Label } from "./ui/label";
 
 const AuthForm = () => {
   return (
-    <form>
-      <div>
+    <form className="space-y-2">
+      <div className="space-y-1">
         <Label htmlFor="email">Email</Label>
         <Input type="email" id="email" />
       </div>
 
-      <div>
+      <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input type="password" id="password" />
       </div>
