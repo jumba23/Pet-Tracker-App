667c19bfecc4aa64280859549b1ed376748e7217
Goran SD <g.cvetic23@gmail.com>
2024-04-13 22:42:17 -0700

    Refactor form inputs in pet-form.tsx to use zod for form validation and update dependencies

---
+++ src/lib/validations.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+import { DEFAULT_PET_IMAGE } from "./constants";
+
+//we are using external validation library zod to validate the form
+export const petFormSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: "Name is required" }).max(100),
+    ownerName: z
+      .string()
+      .trim()
+      .min(1, { message: "Owner Name is required" })
+      .max(100),
+    imageUrl: z.union([
+      z.literal(""),
+      z.string().trim().url({ message: "Image Url is not a valid URL" }),
+    ]),
+    age: z.coerce
+      .number()
+      .int()
+      .positive()
+      .min(0, { message: "Age must be a positive number" }),
+    notes: z.union([z.literal(""), z.string().trim().max(1000)]),
+  })
+  //transform the data before validation - we can customize the data before validation
+  .transform((data) => ({
+    ...data,
+    imageUrl: data.imageUrl.trim() || DEFAULT_PET_IMAGE,
+  }));
+
+// zod will give us the type of the form data
+export type TPetFormData = z.infer<typeof petFormSchema>;
