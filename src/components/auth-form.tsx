37cd1af0157c9e8555f59b143ed16e51bb8b32d7
Goran SD <g.cvetic23@gmail.com>
2024-04-14 08:31:10 -0700

    Refactor AuthForm component to include email and password fields in auth-form.tsx

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -1,5 +1,20 @@
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
 const AuthForm = () => {
-  return <div>auth-form</div>;
+  return (
+    <form>
+      <div>
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" id="email" />
+      </div>
+
+      <div>
+        <Label htmlFor="password">Password</Label>
+        <Input type="password" id="password" />
+      </div>
+    </form>
+  );
 };
 
 export default AuthForm;
