6755365dbcb5a4ca0c06b78a9735aa465004c2d8
Goran SD <g.cvetic23@gmail.com>
2024-04-14 08:23:33 -0700

    Refactor auth layout component to include gap between children in layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import Logo from "@/components/logo";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex flex-col gap-y-5 justify-center items-center min-h-screen">
       <Logo />
       {children}
     </div>
