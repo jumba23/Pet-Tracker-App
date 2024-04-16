8e25b1330c09d4293a26e407ed8049d14050332b
Goran SD <g.cvetic23@gmail.com>
2024-04-15 23:31:54 -0700

    Add config object to middleware in src/middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,3 +4,7 @@ export const middleware = (request: Request) => {
   console.log("middleware", request.url);
   return NextResponse.next();
 };
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
