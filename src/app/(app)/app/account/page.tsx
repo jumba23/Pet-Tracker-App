5c5a4ad839ac5005745b325e51ca1913102a92a1
Goran SD <g.cvetic23@gmail.com>
2024-04-17 11:46:30 -0700

    Add SignOutBtn component and update account/page.tsx to include sign out functionality

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,6 +1,8 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import SignOutBtn from "@/components/sign-out-btn";
 import auth from "@/middleware";
+import { signOut } from "next-auth/react";
 
 const Page = async () => {
   const session = await auth();
@@ -13,12 +15,22 @@ const Page = async () => {
     };
   }
 
+  // ================ EXAMPLE ================
+  // instead of creating this function in server actions component, we can create the function here
+  // you cannot do this in a Client component such as SignOutBtn
+  // const logout = async () => {
+  //   "use server";
+  //   await signOut();
+  // };
+
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
 
-      <ContentBlock className="h-[500px] flex items-center justify-center">
+      <ContentBlock className="h-[500px] flex flex-col gap-3 items-center justify-center">
         <p>Logged in as {session.user.email}</p>
+
+        <SignOutBtn />
       </ContentBlock>
     </main>
   );
---
+++ src/components/sign-out-btn.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Button } from "./ui/button";
+
+const SignOutBtn = () => {
+  return <Button onClick={() => {}}>Sign Out</Button>;
+};
+
+export default SignOutBtn;
