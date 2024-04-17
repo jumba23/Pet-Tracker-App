e3c815118f5f4c8cd46e587a3c27f4269d7a5ffc
Goran SD <g.cvetic23@gmail.com>
2024-04-17 11:35:29 -0700

    Update authentication logic in account/page.tsx to include login authorization

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,13 +1,24 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import auth from "@/middleware";
 
-const Page = () => {
+const Page = async () => {
+  const session = await auth();
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
 
       <ContentBlock className="h-[500px] flex items-center justify-center">
-        <p>Logged in as ...</p>
+        <p>Logged in as {session?.user.email}</p>
       </ContentBlock>
     </main>
   );
