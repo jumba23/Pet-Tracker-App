9b000bbec0398149078cd09618352f3ee06a32ed
Goran SD <g.cvetic23@gmail.com>
2024-04-17 11:27:16 -0700

    Refactor defaultValues assignment in PetForm component to handle different action types

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -29,13 +29,16 @@ const PetForm = ({ actionType, onFormSubmission }: PetFormProps) => {
   } = useForm<TPetFormData>({
     //this is we are connecting external validation library zod to react-hook-form
     resolver: zodResolver(petFormSchema),
-    defaultValues: {
-      name: selectedPet?.name,
-      ownerName: selectedPet?.ownerName,
-      imageUrl: selectedPet?.imageUrl,
-      age: selectedPet?.age,
-      notes: selectedPet?.notes,
-    },
+    defaultValues:
+      actionType === "edit"
+        ? {
+            name: selectedPet?.name,
+            ownerName: selectedPet?.ownerName,
+            imageUrl: selectedPet?.imageUrl,
+            age: selectedPet?.age,
+            notes: selectedPet?.notes,
+          }
+        : undefined,
   });
 
   return (
