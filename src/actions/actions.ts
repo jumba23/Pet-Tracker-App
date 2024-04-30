1b45827be908c6acfde8a7051e45b8a069c2f706
Goran SD <g.cvetic23@gmail.com>
2024-04-30 15:13:29 -0700

    Refactor authentication form in actions.ts to improve form data handling and validation

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -46,16 +46,33 @@ export const logOut = async () => {
   await signOut({ redirectTo: "/" });
 };
 
-export const signUp = async (formData: FormData) => {
-  const hashedPassword = await bcrypt.hash(
-    formData.get("password") as string,
-    10
-  );
+export const signUp = async (formData: unknown) => {
+  //check if formData is a FormData type
+  if (!(formData instanceof FormData)) {
+    return {
+      message: "Invalid form data.",
+    };
+  }
+
+  //convert formData to a plain object
+  const formDataEntries = Object.fromEntries(formData.entries());
+
+  //validation
+  const validatedFormData = authSchema.safeParse(formDataEntries);
+  if (!validatedFormData.success) {
+    return {
+      message: "Invalid form data.",
+    };
+  }
+
+  const { email, password } = validatedFormData.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   // create new user in db using prisma
   await prisma.user.create({
     data: {
-      email: formData.get("email") as string,
+      email,
       hashedPassword,
     },
   });
