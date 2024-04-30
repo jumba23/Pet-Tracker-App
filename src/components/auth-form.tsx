21a777a9104ed365528cb187a776558f2f1c03ab
Goran SD <g.cvetic23@gmail.com>
2024-04-30 14:36:23 -0700

    Refactor authentication form in auth-form.tsx to include required and maxLength attributes for email and password inputs

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -14,12 +14,18 @@ const AuthForm = ({ type }: AuthFormProps) => {
     >
       <div className="space-y-1">
         <Label htmlFor="email">Email</Label>
-        <Input type="email" name="email" id="email" />
+        <Input type="email" name="email" id="email" required maxLength={50} />
       </div>
 
       <div className="mb-4 mt-2 space-y-1">
         <Label htmlFor="password">Password</Label>
-        <Input type="password" name="password" id="password" />
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          required
+          maxLength={50}
+        />
       </div>
 
       <Button>{type === "login" ? "Log in" : "Sign up"}</Button>
